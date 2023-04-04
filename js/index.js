dd9e1f97b805aea4a8400f120964bc1a9173eed0
Khalid <khaliddakar001@gmail.com>
2023-04-05 02:07:11 +0300

    second commit

--- js/index.js
+++ js/index.js
@@ -1,44 +1,50 @@
-const menuContainer = document.getElementById("menu-container");
+const apiUrl = 'https://api.alquran.cloud/v1';
+const chapterList = document.getElementById('chapter-list');
+const quranText = document.getElementById('quran-text');
+const requestOptions = {
+  method: 'GET',
+  redirect: 'follow'
+};
 
-async function fetchFruits() {
-    try {
-        const response = await fetch("https://www.fruityvice.com/api/fruit/all");
-        const fruits = await response.json();
-        return fruits;
-    } catch (error) {
-        console.error(error);
-    }
+function fetchChapterList() {
+  const url = `${apiUrl}/surah`;
+
+  fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      const chapters = data.data;
+      chapters.forEach(chapter => {
+        const link = document.createElement('a');
+        link.href = `#${chapter.number}`;
+        link.innerText = `${chapter.number}. ${chapter.englishName} (${chapter.name})`;
+        link.onclick = () => fetchQuranText(chapter.number);
+        chapterList.appendChild(link);
+      });
+    })
+    .catch(error => console.error(error));
 }
 
-function createMenuItem(fruit) {
-    const menuItem = document.createElement("div");
-    menuItem.classList.add("menu-item");
+function fetchQuranText(chapter) {
+  const url = `${apiUrl}/surah/${chapter}/en.asad,ar.alafasy`;
 
-    const image = document.createElement("img");
-    image.src = fruit.image;
-    image.alt = fruit.name;
-    menuItem.appendChild(image);
-
-    const text = document.createElement("div");
-
-    const name = document.createElement("h2");
-    name.textContent = fruit.name;
-    text.appendChild(name);
-
-    const description = document.createElement("p");
-    description.textContent = fruit.nutrition;
-    text.appendChild(description);
-
-    menuItem.appendChild(text);
-
-    menuContainer.appendChild(menuItem);
+  fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+      const verses = data.data.ayahs;
+      let quranText = '';
+      verses.forEach(verse => {
+        quranText += `${verse.text} (${verse.numberInSurah})<br>`;
+      });
+      quranText = `<h2>${data.data.englishName} (${data.data.name})</h2>${quranText}`;
+      quranText = quranText.replaceAll('<p>', '').replaceAll('</p>', ''); // remove <p> tags from the text
+      quranText = quranText.replaceAll('<br>', '<br><br>'); // add some spacing between verses
+      quranText = quranText.replaceAll('    ', '&nbsp;&nbsp;&nbsp;&nbsp;'); // replace spaces with non-breaking spaces for indentation
+      quranText = quranText.trim(); // remove leading/trailing white space
+      quranText = `<div class="quran-chapter">${quranText}</div>`;
+      quranText.scrollTop = 0; // scroll to the top of the chapter
+      quranText.innerHTML = quranText;
+    })
+    .catch(error => console.error(error));
 }
 
-async function main() {
-    const fruits = await fetchFruits();
-    fruits.forEach(fruit => {
-        createMenuItem(fruit);
-    });
-}
-
-main();
+fetchChapterList(); // fetch the list of chapters on page load
